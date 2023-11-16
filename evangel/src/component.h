95ec2f98ee35888c0addd3ef739376a8b2f9645e
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2023-11-16 21:04:53 +0800

    Add basic component

--- evangel/src/component.h
+++ evangel/src/component.h
@@ -2,13 +2,23 @@
 #define __COMPONENT_H
 
 #include "basic.h"
+#include "map.h"
+#include "vec.h"
+
+// forward declaration
+struct ComponentNode;
 
 typedef struct {
   usize id;
+} ComponentType;
+
+typedef struct {
+  struct ComponentNode *ptr;
 } Entity;
 
 typedef struct {
-
+  // despawn and free up memory (*self)
+  void (*despawn)(void *self, Entity entity);
 } VComponent;
 
 typedef struct {
@@ -16,6 +26,37 @@ typedef struct {
   void *self;
 } PComponent;
 
-struct CComponent {};
+struct ComponentNode {
+  bool is_despawn;
+  ComponentType ty;
+  PComponent component;
+  Entity parent;
+  Vec(Entity) children;
+};
+
+struct ComponentTree {
+  // a lazy clean up map
+  Vec(Vec(struct ComponentNode *)) typeid_component_node_map;
+  // typeid map
+  // Map(ComponentType) entity_typeid_map;
+};
+
+struct CComponent {
+  ComponentType (*add_new_type)();
+
+  Entity (*spawn)(ComponentType typeid, PComponent component);
+
+  void (*add_child)(Entity parent, Entity child);
+
+  void (*despawn)(Entity entity);
+};
+
+extern const struct CComponent CComponent;
+
+#define AddComponentType(T)                                                    \
+  ComponentType ComponentType##T;                                              \
+  void add_component_type_##T() {                                              \
+    ComponentType##T = CComponent.add_new_type();                              \
+  }
 
 #endif // __COMPONENT_H
